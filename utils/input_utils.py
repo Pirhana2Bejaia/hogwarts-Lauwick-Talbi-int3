be38578620655ad2f3b822ec7bc3c7c55931b815
zorban-alt <lauwickadrien@gmail.com>
2025-12-11 17:29:20 +0100

    last update

--- utils/input_utils.py
+++ utils/input_utils.py
@@ -6,15 +6,14 @@ on vérifie que le message ne contient pas que des espaces blancs
 
 
 def ask_text(message):
+
     ask = input(message)
 
     checker = "not check"
 
     if len(ask) == 0:
         return ask_text(message)
-
     else:
-
         for i in range(len(ask)):
             if ask[i] != " ":
                 checker = "check"
@@ -23,3 +22,78 @@ def ask_text(message):
             return ask_text(message)
         else:
             return '✅'
+
+def ask_choice(message,options):
+    min = 1
+    max = len(option)
+    print(message)
+    print()
+    for i in range(len(options)):
+        print(i+1,option[i])
+    return ask_number(message,min,max)
+
+
+def ask_number(message, min_val=None, max_val=None):
+    check = 'ok'
+    answer = input(message)
+
+    if len(answer) == 0:
+        check = 'error'
+
+    if len(answer) != 0:
+        if answer[0] == '-':
+            negative = 'true'
+            answer = answer[1:]
+
+    else:
+        negative = 'false'
+
+    for i in range(len(answer)):
+        if ord(answer[i]) > 57 or ord(answer[i]) < 48:
+            check = 'error'
+
+    if check == 'ok':
+        if negative == 'true':
+            answer = -int(answer)
+        else:
+            answer = int(answer)
+
+    if check == 'ok':
+        if min_val != None:
+            if min_val > answer or answer > max_val:
+                check = 'error'
+
+    while check == 'error':
+        print("Please enter a number between", min_val, "and", max_val, ".")
+        answer = input(message)
+        check = 'ok'
+
+        if len(answer) == 0:
+            check = 'error'
+
+        if len(answer) != 0:
+            if answer[0] == '-':
+                negative = 'true'
+                answer = answer[1:]
+            else:
+                negative = 'false'
+
+        for i in range(len(answer)):
+            if ord(answer[i]) > 57 or ord(answer[i]) < 48:
+                check = 'error'
+
+        if check == 'ok':
+            if negative == 'true':
+                answer = -int(answer)
+            else:
+                answer = int(answer)
+
+        if check == 'ok':
+            if min_val != None and max_val != None:
+                if min_val > answer or answer > max_val:
+                    check = 'error'
+
+    return answer
+
+
+print(ask_number('votre age : ', -15, 15))
